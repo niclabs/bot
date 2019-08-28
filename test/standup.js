c3b89cf648e36087965f11d6aa3a9566235465c7
Felipe Lalanne <flalanne@niclabs.cl>
2019-08-28 18:25:28 -0400

    Add standup tests

--- test/standup.js
+++ test/standup.js
@@ -45,6 +45,16 @@ describe('Standup meeting launch', () => {
     return updates.result[0].message;
   };
 
+  const sendMessageWithMultipleReplies = async (client, msg) => {
+    await client.sendMessage(client.makeMessage(msg));
+    const updates = await client.getUpdates();
+    if (updates.result.length < 1) {
+      assert.fail(`no reply for message ${JSON.stringify(msg)}`);
+    }
+
+    return updates.result;
+  };
+
   it('should reply in private chat when called from group', async () => {
     // Get client for a group chat
     const client = server.getClient(token, { chatId: 2, type: 'group' });
@@ -119,13 +129,32 @@ describe('Standup meeting launch', () => {
     assert.strictEqual(message.reply_markup.keyboard[0][0], 'Test Name');
   });
 
+  it('should start standup immediately in selected group', async () => {
+    // Get client for a group chat
+    const groupClient = server.getClient(token, { chatId: 2, type: 'group' });
+    let message = await sendCommand(groupClient, '/start');
+
+    // Check that reply was in privaate chat
+    assert.strictEqual(message.chat_id, 1);
+
+    const privateClient = server.getClient(token);
+    message = await sendCommand(privateClient, '/standup');
+
+    // Reply to group selection
+    message = await sendMessage(privateClient, message.reply_markup.keyboard[0][0]);
+    assert.strictEqual(
+      message.text,
+      `1. ¿Que hiciste ayer? 
+            
+_Ojo: puedes usar Ctrl+enter para escribir mútiples líneas_`,
+      'expected question 1 to standup',
+    );
+  });
+
   it('should start standup in private list when user has identified', async () => {
     // Get client for a group chat
     const privateClient = server.getClient(token);
     let message = await sendCommand(privateClient, '/start');
-
-    // Check that reply was in privaate chat
-    assert.strictEqual(message.chat_id, 1);
 
     assert.strictEqual(
       message.text,
@@ -134,6 +163,11 @@ describe('Standup meeting launch', () => {
 
     const groupClient = server.getClient(token, { chatId: 2, type: 'group' });
     message = await sendCommand(groupClient, '/standup');
+
+    // Check that reply was in private chat
+    assert.strictEqual(message.chat_id, 1);
+
+    // Check standup staart
     assert.strictEqual(
       message.text,
       "Es hora de iniciar el standup para el equipo 'Test Name'. Son sólo 3 preguntas. ¿Vamos?",
@@ -145,5 +179,139 @@ describe('Standup meeting launch', () => {
       'expected array with two options',
     );
     assert.strictEqual(message.reply_markup.keyboard[0][0], '👍 vamos!');
+  });
+
+  it('should ask 3 questions for standup', async () => {
+    // Get client for a group chat
+    const privateClient = server.getClient(token);
+    let message = await sendCommand(privateClient, '/start');
+
+    const groupClient = server.getClient(token, { chatId: 2, type: 'group' });
+    message = await sendCommand(groupClient, '/standup');
+
+    // Check that reply was in private chat
+    assert.strictEqual(message.chat_id, 1);
+
+    // Start standup
+    message = await sendMessage(privateClient, message.reply_markup.keyboard[0][0]);
+    assert.strictEqual(
+      message.text,
+      `1. ¿Que hiciste ayer? 
+          
+_Ojo: puedes usar Ctrl+enter para escribir mútiples líneas_`,
+      'expected question 1 to standup',
+    );
+
+    // Reply to question 1
+    message = await sendMessage(privateClient, 'aaaaaa');
+    assert.strictEqual(message.text, '2. ¿Que harás hoy?', 'expected question 2 to standup');
+
+    // Reply to question 2
+    message = await sendMessage(privateClient, 'bbbbbb');
+    assert.strictEqual(
+      message.text,
+      '3. ¿Tienes algún obstáculo para avanzar?',
+      'expected question 3 to standup',
+    );
+
+    // Reply to question 3
+    message = await sendMessage(privateClient, 'cccccc');
+
+    // Check that responses are inside reply text
+    if (
+      !message.text.includes('aaaaaa')
+      || !message.text.includes('bbbbbb')
+      || !message.text.includes('cccccc')
+    ) {
+      assert.fail('expected replies inside standup reply text');
+    }
+
+    assert.strictEqual(
+      message.reply_markup.keyboard[0].length,
+      2,
+      'expected array with two options',
+    );
+    assert.strictEqual(message.reply_markup.keyboard[0][0], '👍 dale!');
+
+    // Send ok reply
+    const results = await sendMessageWithMultipleReplies(
+      privateClient,
+      message.reply_markup.keyboard[0][0],
+    );
+
+    assert.strictEqual(results.length, 2, 'expected two replies');
+
+    // First Reply should be in private chat
+    let index = 0;
+    if (results[0].message.chat_id !== 1 && results[1].message.chat_id !== 1) {
+      assert.fail('expected one of the replies to be in private chat');
+    } else {
+      index = results[0].message.chat_id === 1 ? 0 : 1;
+      assert.strictEqual(results[index].message.text, 'Hecho!');
+    }
+
+    index = (index + 1) % 2;
+    if (results[0].message.chat_id !== 2 && results[1].message.chat_id !== 2) {
+      assert.fail('expected one of the replies to be in group chat');
+    } else if (
+      !results[index].message.text.includes('aaaaaa')
+      || !results[index].message.text.includes('bbbbbb')
+      || !results[index].message.text.includes('cccccc')
+    ) {
+      assert.fail('expected replies inside standup reply text');
+    }
+
+    // Only one standup per day
+    message = await sendCommand(groupClient, '/standup');
+    assert.strictEqual(
+      message.text,
+      "Ya hiciste el standup de hoy para el equipo 'Test Name'. Felicitaciones!",
+    );
+  });
+
+  it('should accept /cancel', async () => {
+    // Get client for a group chat
+    const privateClient = server.getClient(token);
+    let message = await sendCommand(privateClient, '/start');
+    const groupClient = server.getClient(token, { chatId: 2, type: 'group' });
+    message = await sendCommand(groupClient, '/standup');
+
+    // Check that reply was in private chat
+    assert.strictEqual(message.chat_id, 1);
+
+    // Start standup
+    message = await sendMessage(privateClient, message.reply_markup.keyboard[0][0]);
+    message = await sendCommand(privateClient, '/cancel');
+    assert.strictEqual(message.text, 'OK');
+  });
+
+  it('should allow user to refuse standup', async () => {
+    // Get client for a group chat
+    const privateClient = server.getClient(token);
+    let message = await sendCommand(privateClient, '/start');
+    const groupClient = server.getClient(token, { chatId: 2, type: 'group' });
+    message = await sendCommand(groupClient, '/standup');
+
+    // Check that reply was in private chat
+    assert.strictEqual(message.chat_id, 1);
+
+    // Start standup
+    message = await sendMessage(privateClient, message.reply_markup.keyboard[0][1]);
+    assert.strictEqual(message.text, 'No hay problema');
+  });
+
+  it('should only accept keyboard messages', async () => {
+    // Get client for a group chat
+    const privateClient = server.getClient(token);
+    let message = await sendCommand(privateClient, '/start');
+    const groupClient = server.getClient(token, { chatId: 2, type: 'group' });
+    message = await sendCommand(groupClient, '/standup');
+
+    // Check that reply was in private chat
+    assert.strictEqual(message.chat_id, 1);
+
+    // Start standup
+    message = await sendMessage(privateClient, 'Lalalala');
+    assert.strictEqual(message.text, 'Lo siento, no entendí tu respuesta');
   });
 });
